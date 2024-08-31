5b053bc51495b5e867f7e27f41f7e5d97876c37b
root <root@DESKTOP-EC0C6O5>
2024-08-31 15:27:42 +0200

    Black screen opens and closes.

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import pygame
+from constants import *
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    i = 1
+    while i < 5:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+              return
+        pygame.Surface.fill(screen, color = (0,0,0))
+        pygame.display.flip()
     print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(F"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ =="__main__":
     main()
